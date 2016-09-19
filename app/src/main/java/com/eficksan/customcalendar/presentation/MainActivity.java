708329cbc8c6b71e89ccb6566d0ec95bb8ded432
Aleksei_Ivshin <Aleksei_Ivshin@epam.com>
2016-09-19 14:11:49 +0400

    Init project

---
+++ app/src/main/java/com/eficksan/customcalendar/presentation/MainActivity.java
@@ -0,0 +1,15 @@
+package com.eficksan.customcalendar.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eficksan.customcalendar.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
